d454963f17ec357e55b693e006c82a201a608c47
Mate Kovacs <mkovaxx@gmail.com>
2025-01-07 19:31:54 +0100

    simple app skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    font: String,
+
+    #[arg(short, long, value_name = "FILE", default_value = "output")]
+    output: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let chars: String = ('A'..'Z').chain('0'..'9').collect();
+
+    for char_1 in chars.chars() {
+        for char_2 in chars.chars() {
+            let name = format!("{char_1}{char_2}");
+            println!("Preparing shape for {name}...");
+        }
+    }
 }
